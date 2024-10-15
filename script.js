e5d015a98de02034d1707d06fc627f417fb1f6ed
Javier Preciado <jpreciado85@gmail.com>
2024-10-15 15:49:38 -0500

    video y efecto parallax

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", function() {
+        document.querySelectorAll("nav ul li a").forEach(item => item.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
